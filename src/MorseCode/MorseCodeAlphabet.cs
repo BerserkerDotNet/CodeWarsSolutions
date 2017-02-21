539e98cafdb693a69d28b9f400d6b0b72022146d
Andrii Snigyr (Globallogic Inc) <v-ansnig@microsoft.com>
2017-02-20 22:46:32 -0800

    Morse challange http://www.codewars.com/kata/decode-the-morse-code part 1

---
+++ src/MorseCode/MorseCodeAlphabet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace MorseCode
+{
+    public static class MorseCodeAlphabet
+    {
+        private static Dictionary<string, string> _alphabet = new Dictionary<string, string>
+        {
+            { ".-", "A" },
+            { "-...", "B" },
+            { "-.-.", "C" },
+            { "-..", "D" },
+            { ".", "E" },
+            { "..-.", "F" },
+            { "--.", "G" },
+            { "....", "H" },
+            { "..", "I" },
+            { ".---", "J" },
+            { "-.-", "K" },
+            { ".-..", "L" },
+            { "--", "M" },
+            { "-.", "N" },
+            { "---", "O" },
+            { ".--.", "P" },
+            { "--.-", "Q" },
+            { ".-.", "R" },
+            { "...", "S" },
+            { "-", "T" },
+            { "..-", "U" },
+            { "...-", "V" },
+            { ".--", "W" },
+            { "-..-", "X" },
+            { "-.--", "Y" },
+            { "--..", "Z" },
+            { ".----", "1" },
+            { "..---", "2" },
+            { "...--", "3" },
+            { "....-", "4" },
+            { ".....", "5" },
+            { "-....", "6" },
+            { "--...", "7" },
+            { "---..", "8" },
+            { "----.", "9" },
+            { "-----", "0" },
+            { "...---...", "SOS" },
+        };
+
+        public static string GetSymbol(string morse)
+        {
+            if (_alphabet.ContainsKey(morse))
+            {
+                return _alphabet[morse];
+            }
+
+            throw new KeyNotFoundException($"{morse} is unknown morse character.");
+        }
+    }
+}
---
+++ tests/KataTests/MorseTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class MorseTests
+    {
+        [TestCase(".... . -.--   .--- ..- -.. .", "HEY JUDE")]
+        [TestCase("...---...", "SOS")]
+        [TestCase(".... . .-.. .-.. ---   .-- --- .-. .-.. -..", "HELLO WORLD")]
+        [TestCase("  .. .    ", "IE")]
+        public void Morse_Basic(string morse, string expected)
+        {
+            var result = MorseCode.MorseCode.Decode(morse);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
