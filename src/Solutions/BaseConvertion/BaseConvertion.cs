d557147f7b5e2ee049f2e23e54719a5a16a27fe4
Andrii Snihyr <war_chief@bigmir.net>
2018-04-01 15:52:25 -0700

    Solution to "Base converter" Kata https://www.codewars.com/kata/base-conversion/train/csharp

---
+++ src/Solutions/BaseConvertion/BaseConvertion.cs
@@ -0,0 +1,134 @@
+﻿using Solutions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions.BaseConvertion
+{
+    /*
+     * In this kata you have to implement a base converter, which converts positive integers between arbitrary bases / alphabets.
+     * The function convert() should take an input (string), the source alphabet (string) and the target alphabet (string). You can assume that the input value always consists of characters from the source alphabet. You don't need to validate it.
+     */
+    public class BaseConvertion : ISolution
+    {
+        public string DisplayName => "Base convertion";
+
+        public void Execute(IHost host)
+        {
+            var input = host.Read<string>("Enter a number to convert:");
+
+            var alphabetsChoise = new StringBuilder();
+            var idx = 0;
+            foreach (var alphabet in Alphabet.All)
+            {
+                alphabetsChoise.AppendLine($"{idx}. {alphabet.Key}");
+                idx++;
+            }
+
+            var alphabets = Alphabet.All.ToArray();
+            host.Show("From alphabet:");
+            var alphabetFromIdx = host.Read<int>(alphabetsChoise.ToString());
+            var alphabetFrom = alphabets[alphabetFromIdx].Value;
+
+            host.Show("To alphabet:");
+            var alphabetToIdx = host.Read<int>(alphabetsChoise.ToString());
+            var alphabetTo = alphabets[alphabetToIdx].Value;
+
+            var result = Convert(input, alphabetFrom, alphabetTo);
+
+            host.Show($"Result: {result}");
+        }
+
+        public string Convert(string input, string sourceAlphabet, string targetAlphabet)
+        {
+            CheckIfNotNull(input, nameof(input));
+            CheckIfNotNull(sourceAlphabet, nameof(sourceAlphabet));
+            CheckIfNotNull(targetAlphabet, nameof(targetAlphabet));
+
+            if (sourceAlphabet == targetAlphabet)
+                return input;
+
+            if (sourceAlphabet.Length == targetAlphabet.Length)
+                return AlphabetConvert(input, sourceAlphabet, targetAlphabet);
+
+            var inputBase = sourceAlphabet.Length;
+
+            var decimalValue = 0L;
+            var idx = input.Length - 1;
+            foreach (var symbol in input)
+            {
+                var value = sourceAlphabet.IndexOf(symbol);
+                decimalValue += value * (long)Math.Pow(inputBase, idx);
+                idx--;
+            }
+
+            return ChangeBase(decimalValue, targetAlphabet);
+        }
+
+        private string AlphabetConvert(string input, string sourceAlphabet, string targetAlphabet)
+        {
+            var convertedArray = input.ToCharArray()
+                .Select(c => targetAlphabet[sourceAlphabet.IndexOf(c)])
+                .ToArray();
+            return new string(convertedArray);
+        }
+
+        private string ChangeBase(long decimalValue, string alphabet)
+        {
+            if (decimalValue <= alphabet.Length - 1)
+                return alphabet[(int)decimalValue].ToString();
+
+            var targetBase = alphabet.Length;
+            var currentValue = decimalValue;
+            var builder = new StringBuilder();
+            while (currentValue >= targetBase)
+            {
+                var rem = currentValue % targetBase;
+                builder.Append(alphabet[(int)rem]);
+                currentValue = (long)Math.Floor(currentValue / (decimal)targetBase);
+            }
+
+            builder.Append(alphabet[(int)currentValue]);
+
+            return Reverse(builder.ToString());
+        }
+
+        private void CheckIfNotNull(string value, string parameterName = "")
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(parameterName);
+        }
+
+        private string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    public class Alphabet
+    {
+        public const string BINARY = "01";
+        public const string OCTAL = "01234567";
+        public const string DECIMAL = "0123456789";
+        public const string HEXA_DECIMAL = "0123456789ABCDEF";
+        public const string ALPHA_LOWER = "abcdefghijklmnopqrstuvwxyz";
+        public const string ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string ALPHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string ALPHA_NUMERIC = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static Dictionary<string, string> All = new Dictionary<string, string>
+        {
+            { nameof(BINARY), BINARY },
+            { nameof(OCTAL), OCTAL },
+            { nameof(DECIMAL), DECIMAL },
+            { nameof(HEXA_DECIMAL), HEXA_DECIMAL },
+            { nameof(ALPHA_LOWER), ALPHA_LOWER },
+            { nameof(ALPHA_UPPER), ALPHA_UPPER },
+            { nameof(ALPHA), ALPHA },
+            { nameof(ALPHA_NUMERIC), ALPHA_NUMERIC },
+        };
+    }
+}
---
+++ tests/SolutionsTests/BaseConversionTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solutions.BaseConvertion;
+using System;
+
+namespace SolutionsTests
+{
+    [TestFixture]
+    public class BaseConversionTests
+    {
+        BaseConvertion _converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _converter = new BaseConvertion();
+        }
+
+        [TestCase(null, null, null)]
+        [TestCase("", "", "")]
+        [TestCase("", "012", "01")]
+        [TestCase("12", "", "01")]
+        [TestCase("12", "01", "")]
+        public void ShouldThrowIfInvalidInput(string input, string sAlphabet, string tAlphabet)
+        {
+            _converter.Invoking(c => c.Convert(input, sAlphabet, tAlphabet))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+
+        [TestCase("15", Alphabet.DECIMAL, Alphabet.BINARY, "1111")]
+        [TestCase("1111", Alphabet.BINARY, Alphabet.DECIMAL, "15")]
+        [TestCase("15", Alphabet.DECIMAL, Alphabet.HEXA_DECIMAL, "F")]
+        [TestCase("F", Alphabet.HEXA_DECIMAL, Alphabet.DECIMAL, "15")]
+        [TestCase("30A8", Alphabet.HEXA_DECIMAL, Alphabet.DECIMAL, "12456")]
+        [TestCase("30A8", Alphabet.HEXA_DECIMAL, Alphabet.OCTAL, "30250")]
+        [TestCase("hello", Alphabet.ALPHA_LOWER, Alphabet.HEXA_DECIMAL, "320048")]
+        [TestCase("SAME", Alphabet.ALPHA_UPPER, Alphabet.ALPHA_UPPER, "SAME")]
+        [TestCase("LOWER", Alphabet.ALPHA_UPPER, Alphabet.ALPHA_LOWER, "lower")]
+        [TestCase("CodeWars", Alphabet.ALPHA, Alphabet.BINARY, "110100110111011111011110001100111111110000110")]
+        public void ShouldConvertBetweenAlphabets(string input, string sAlphabet, string tAlphabet, string expected)
+        {
+            var result = _converter.Convert(input, sAlphabet, tAlphabet);
+            result.Should().Be(expected);
+        }
+    }
+}
