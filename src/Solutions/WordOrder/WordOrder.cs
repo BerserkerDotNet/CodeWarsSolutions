fa81e9bad000bfc8bb9e81c9717ee959d2e535e2
Andrii Snihyr <war_chief@bigmir.net>
2019-01-21 18:40:51 -0800

    Word Order Kata

---
+++ src/Solutions/WordOrder/WordOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Solutions.WordOrder
+{
+    public class WordOrder
+    {
+        public static string Order(string input)
+        {
+            var arrayOfNumbers = new[] {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            var words = input.Split(' ').OrderBy(r => r[r.IndexOfAny(arrayOfNumbers)]);
+            return string.Join(' ', words);
+        }
+    }
+}
---
+++ tests/SolutionsTests/WordOrder.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solutions.WordOrder;
+
+namespace SolutionsTests
+{
+    [TestFixture]
+    public class WordOrderTests
+    {
+        [Test]
+        public void ShouldReturnEmptyString([Values(null, "")] string input)
+        {
+            var result = WordOrder.Order(input);
+            result.Should().BeEmpty();
+        }
+
+        [TestCase("is2 Thi1s T4est 3a", "Thi1s is2 3a T4est")]
+        [TestCase("4of Fo1r pe6ople g3ood th5e the2", "Fo1r the2 g3ood 4of th5e pe6ople")]
+        [TestCase("3 6 4 2 8 7 5 1 9", "1 2 3 4 5 6 7 8 9")]
+        public void ShouldOrderWords(string input, string expected)
+        {
+            var result = WordOrder.Order(input);
+            result.Should().Be(expected);
+        }
+    }
+}
