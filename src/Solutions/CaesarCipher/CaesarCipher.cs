aed8fb1c3df0d2f461db7b366ad175328f3a2210
Andrii Snihyr <war_chief@bigmir.net>
2019-02-03 18:01:53 -0800

    Ceasar cipher kata

---
+++ src/Solutions/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,90 @@
+﻿using Solutions.Infrastructure;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Solutions.CaesarCipher
+{
+    public class CaesarCipher : ISolution
+    {
+        const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public string DisplayName => "Ceasar Cipher";
+
+        public void Execute(IHost host)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Encrypt(string plainText, int shift)
+        {
+            var resultString = TransformText(plainText, shift, decrypting: false);
+            var chunkLength = (int)Math.Ceiling(resultString.Length / (double)5);
+            var result = new string[5];
+            result[0] = GetSubString(resultString, 0, chunkLength);
+            result[1] = GetSubString(resultString, 1, chunkLength);
+            result[2] = GetSubString(resultString, 2, chunkLength);
+            result[3] = GetSubString(resultString, 3, chunkLength);
+            result[4] = GetSubString(resultString, 4, chunkLength);
+
+            return result;
+        }
+
+        public string Decrypt(string[] parts, int shift)
+        {
+            var cipherText = string.Concat(parts);
+            return TransformText(cipherText, shift, decrypting: true);
+        }
+
+        private string TransformText(string text, int shift, bool decrypting)
+        {
+            var sb = new StringBuilder(text.Length);
+            for (int i = 0; i < text.Length; i++)
+            {
+                var character = text[i];
+                if (!char.IsLetter(character))
+                {
+                    sb.Append(character);
+                }
+                else
+                {
+                    var isLower = char.IsLower(character);
+                    var currentPosition = Alphabet.IndexOf(char.ToUpper(character));
+                    var shiftedPosition = decrypting ? CalculateOpositePosition(currentPosition, shift) : CalculatePosition(currentPosition, shift);
+                    var shiftedChar = isLower ? char.ToLower(Alphabet[shiftedPosition]) : Alphabet[shiftedPosition];
+                    sb.Append(shiftedChar);
+                }
+
+                shift++;
+            }
+            return sb.ToString();
+        }
+
+        private string GetSubString(string s, int partNumber, int length)
+        {
+            var startIndex = length * partNumber;
+            if (startIndex + length >= s.Length)
+            {
+                length = s.Length - startIndex;
+            }
+
+            if (length <= 0)
+            {
+                return string.Empty;
+            }
+
+            return s.Substring(startIndex, length);
+        }
+
+        private int CalculatePosition(int currentPosition, int shiftValue)
+        {
+            return (currentPosition + shiftValue) % Alphabet.Length;
+        }
+
+        private int CalculateOpositePosition(int currentPosition, int shiftValue)
+        {
+            int modulo = (currentPosition - shiftValue) % Alphabet.Length;
+            return (modulo < 0) ? modulo + Alphabet.Length : modulo;
+        }
+    }
+}
---
+++ tests/SolutionsTests/CeasarCipherTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solutions.CaesarCipher;
+
+namespace SolutionsTests
+{
+    [TestFixture]
+    public class CeasarCipherTests
+    {
+        [TestCaseSource(nameof(Data))]
+        public void ShouldEncryptCorrectly(string input, string[] expected)
+        {
+            var cipher = new CaesarCipher();
+            var result = cipher.Encrypt(input, 1);
+
+            result.Should().BeEquivalentTo(expected, opt => opt.WithStrictOrdering());
+        }
+
+        [TestCaseSource(nameof(Data))]
+        public void ShouldDecryptCorrectly(string expected, string[] parts)
+        {
+            var cipher = new CaesarCipher();
+            var result = cipher.Decrypt(parts, 1);
+
+            result.Should().Be(expected);
+        }
+
+        private static object[] Data =
+        {
+            new object[]
+            {
+                "I should have known that you would have a perfect answer for me!!!",
+                new[] { "J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!" }
+            },
+            new object[]
+            {
+                "I s",
+                new[] { "J", " ", "v", "", "" }
+            },
+            new object[]
+            {
+                "I",
+                new[] { "J", "", "", "", "" }
+            }
+        };
+
+    }
+}
