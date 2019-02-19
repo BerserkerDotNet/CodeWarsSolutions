844178c7884e615d55d01045013d18cf9310e123
Andrii Snihyr <war_chief@bigmir.net>
2019-02-18 19:16:53 -0800

    Square into squares Kata

---
+++ src/Solutions/SquareIntoSquares/SquareIntoSquares.cs
@@ -0,0 +1,74 @@
+﻿using Solutions.Infrastructure;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Solutions.SquareIntoSquares
+{
+    public class SquareIntoSquares : ISolution
+    {
+        public string DisplayName => "Square into Squares";
+
+        public void Execute(IHost host)
+        {
+            var n = host.Read<long>("Enter number:");
+            var result = Decompose(n);
+
+            host.Show($"Result: {result}");
+        }
+
+        public string Decompose(long n)
+        {
+            var sb = new StringBuilder();
+
+            if (!FindRoot(n, n, Sqr(n), sb))
+            {
+                return null;
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public bool FindRoot(long n, long prevX, double sum, StringBuilder sb)
+        {
+            if (sum == 0)
+            {
+                return true;
+            }
+
+            var x = long.MaxValue;
+            var isValid = false;
+            x = GetRoot(sum);
+            while (x > 0)
+            {
+                if (x == n)
+                {
+                    x--;
+                }
+
+                if (x >= prevX)
+                {
+                    return false;
+                }
+
+                var nSum = sum - Sqr(x);
+                isValid = FindRoot(n, x, nSum, sb);
+                if (isValid)
+                    break;
+
+                x--;
+            }
+
+            if (isValid)
+            {
+                sb.Append($"{x} ");
+            }
+
+            return isValid;
+        }
+
+        private long GetRoot(double val) => (long)Math.Floor(Math.Sqrt(val));
+
+        private double Sqr(double number) => Math.Pow(number, 2);
+    }
+}
---
+++ tests/SolutionsTests/SquareIntoSquaresTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Solutions.SquareIntoSquares;
+
+namespace SolutionsTests
+{
+    [TestFixture]
+    public class SquareIntoSquaresTests
+    {
+        [TestCase(11, "1 2 4 10")]
+        [TestCase(50, "1 3 5 8 49")]
+        [TestCase(70, "1 2 3 5 10 69")]
+        [TestCase(12, "1 2 3 7 9")]
+        [TestCase(625, "2 5 8 34 624")]
+        [TestCase(7100, "2 3 5 119 7099")]
+        [TestCase(1234567, "2 8 32 1571 1234566")]
+        [TestCase(6, null)]
+        public void ShouldBreakDownSequance(long number, string expected)
+        {
+            var solution = new SquareIntoSquares();
+            var result = solution.Decompose(number);
+
+            result.Should().Be(expected);
+        }
+    }
+}
